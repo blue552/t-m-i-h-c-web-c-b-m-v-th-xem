125cf6aafa0346b8b87091bb31fc643573e3dc3b
Nguyễn Đức Bảo <119411591+blue552@users.noreply.github.com>
2024-01-28 23:58:03 +0700

    Add files via upload
    create card birthday

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Mai Bình Minh';
+const giftUrl = 'https://www.facebook.com/maibinhminh291';
+const eventName = 'Happy Birthday';
+const titleCard = 'From: Nguyễn Đức Bảo';
+const contentCard = 'Chúc c sinh nhật vui vẻ, sang tuổi mới chạy dl ít hơn,đi làm về sớm hơn, học giỏi tốt hơn và thân thiện hơn nha =))';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'anh.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
